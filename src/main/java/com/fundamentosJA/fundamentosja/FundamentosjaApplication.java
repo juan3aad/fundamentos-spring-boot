ea9e5f2ff1b9c245e6cf7b0ce9d7edcabf2d989b
Alexander Agreda <juan3aad@gmail.com>
2021-08-03 20:46:14 -0500

    primer commit

---
+++ src/main/java/com/fundamentosJA/fundamentosja/FundamentosjaApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosJA.fundamentosja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosJA/fundamentosja/FundamentosjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosJA.fundamentosja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
