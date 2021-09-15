1f4ed619b7347e043818ca48d126e59f63d06737
Alexander Agreda <juan3aad@gmail.com>
2021-09-14 23:19:07 -0500

    Cambio de puerto y path

---
+++ src/main/java/com/fundamentosJA/fundamentosja/bean/MyBeanTresImplement.java
@@ -0,0 +1,9 @@
+package com.fundamentosJA.fundamentosja.bean;
+
+public class MyBeanTwoImplement implements MyBean {
+
+    @Override
+    public void print() {
+        System.out.println("Hola desde mi implementación propia desde el bean TWO");
+    }
+}
